17b946e8336b487e37941f1e129444a0a607642a
Ivan <34632454+abreulima@users.noreply.github.com>
2023-05-02 23:50:42 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const typeDefs = `#graphql
+  type Query {
+    greering: String
+  }
+`;
+
+ const resolvers = {
+  Query: {
+    greeting: () => 'Hello World',
+  },
+ };
+ 
+ const server = new ApolloServer({typeDefs, resolvers});
+ const info = await startStandaloneServer(server, { listen: { port: 9000 } });
